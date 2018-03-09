6da4702d3814631bc40e942ad56da05542d0f226
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-03-09 11:20:31 +0100

    Add initial set of solutions

--- report/main.py
+++ report/main.py
@@ -37,59 +37,116 @@ def _parse_arguments():
     return args
 
 
+def store_address(db, pdf):
+    ADDRESS_SQL = """
+      select s.store_id, a.address, c.city, cn.country
+        from store as s
+        join address as a on s.address_id = a.address_id
+        join city as c on c.city_id = a.city_id
+        join country as cn on cn.country_id = c.country_id
+       limit 1;
+    """
+    address = db.execute(ADDRESS_SQL)
+    pdf.set_store_address(address[0][1:])
+
+
+def rentals_by_day(db, pdf, wb, start_date, end_date):
+    RENTALS_BY_DAY_SQL = """
+      select date_trunc('day', r.rental_date) as day, count(*) as n
+        from rental as r
+       where r.rental_date >= %(start)s and r.rental_date < %(end)s
+    group by day
+    order by day;
+    """
+    rentals = db.execute(RENTALS_BY_DAY_SQL,
+                         dict(start=start_date, end=end_date))
+    rentals_dict = {d.day: n for d, n in rentals}
+    days = list(range(1, 32))
+    n_rents = [rentals_dict.get(d, 0) for d in days]
+
+    pdf.add_paragraph("Number of rentals per day in Jun 2005:")
+    pdf.add_line_chart(170, 50, [str(d) for d in days], [n_rents])
+
+    ws = wb.create_sheet("rentals")
+    ws["A1"] = "Day"
+    ws["B1"] = "No Rentals"
+    for row in zip(days, n_rents):
+        ws.append(row)
+
+
+def top_ten_customers_by_count(db, pdf, wb, start_date, end_date):
+    TOP_TEN_CUSTOMERS_BY_COUNT = """
+      select c.first_name, c.last_name, count(c.customer_id) as n
+        from customer as c
+        join rental as r on r.customer_id = c.customer_id
+       where r.rental_date >= %s and r.rental_date < %s
+    group by c.customer_id
+    order by n desc
+       limit 10;
+    """
+    top_customers = db.execute(TOP_TEN_CUSTOMERS_BY_COUNT,
+                               (start_date, end_date))
+    top_customers_str = [
+        (fn, ln, str(cnt)) for fn, ln, cnt in top_customers
+    ]
+    rows = [
+        ("First Name", "Last Name", "Count"),
+    ] + top_customers_str
+    sizes = 60, 60, 20
+
+    pdf.add_paragraph("Top 10 customers (by number of rentals) for Jun 2005:")
+    pdf.add_table(rows, sizes)
+
+    ws = wb.create_sheet("customers")
+    ws.append(rows[0])
+    for i, (fn, ln, n) in enumerate(top_customers, 2):
+        ws.cell(i, 1, fn)
+        ws.cell(i, 2, ln)
+        ws.cell(i, 3, n)
+
+
+def top_ten_actors(db, pdf, wb, start_date, end_date):
+    TOP_TEN_ACTORS = """
+      select a.first_name, a.last_name, count(a.actor_id) as n
+        from actor as a
+        join film_actor as fa on fa.actor_id = a.actor_id
+        join film as f on f.film_id = fa.film_id
+        join inventory as i on i.film_id = f.film_id
+        join rental as r on i.inventory_id = r.inventory_id
+       where r.rental_date >= %(start)s and r.rental_date < %(end)s
+    group by a.actor_id
+    order by n desc
+       limit 10;
+    """
+    actors = db.execute(TOP_TEN_ACTORS,
+                        {"start": start_date, "end": end_date})
+    actor_labels = tuple("{} {}".format(fn, ln) for fn, ln, _ in actors)
+    actor_shares = tuple(a[2] for a in actors)
+
+    pdf.add_paragraph("Top ten actors in Jun 2005:")
+    pdf.add_pie_chart(80, 80, actor_labels, actor_shares, side_labels=True)
+
+    ws = wb.create_sheet("actors")
+    ws.append(("First Name", "Last Name", "Count"))
+    for a in actors:
+        ws.append(a)
+
+
 def main():
     args = _parse_arguments()
 
-    # Database access
     db = Database(args.host, args.port, args.user, args.password, args.dbname)
-    result = db.execute("select * from store")
-    print(result)
-    #http://initd.org/psycopg/docs/usage.html#passing-parameters-to-sql-queries
-    result = db.execute("select * from rental where rental_date < %s",
-                        (datetime.date(2005, 5, 25),))
-    print(result)
-    result = db.execute("select * from rental where rental_date < %(date)s",
-                        {"date": datetime.date(2005, 5, 25)})
-    print(result)
-
-    # Excel writing
     wb = openpyxl.Workbook()
-    ws = wb.active
-    ws.title = "Data"
-    titles = "store_id", "manager_staff_id", "address_id", "last_update"
-    for i, t in enumerate(titles, 1):
-        ws.cell(1, i, t)
-    for i, row in enumerate(result, 2):
-        for j, val in enumerate(row, 1):
-            ws.cell(i, j, val)
-    wb.save("sample.xlsx")
-
-    # PDF generation
     pdf = Pdf("test.pdf")
-    pdf.set_store_address(("Store A", "My road 3", "another line", "City"))
 
-    rows = (
-        ("c1", "c2", "c3"),
-        ("v1", "v2", "v3"),
-    )
-    sizes = 20, 30, 10
-    pdf.add_table(rows, sizes)
-    pdf.add_paragraph("Long text should this be but there is just no time.")
+    start_date = datetime.date(2005, 6, 1)
+    end_date = datetime.date(2005, 7, 1)
 
-    labels = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")
-    data = [
-        (   13,     5,    20,    22,    37,    45,    19,     4),
-        (    5,    20,    46,    38,    23,    21,     6,    14),
-    ]
-    pdf.add_line_chart(140, 70, labels, data)
-    pdf.add_paragraph("Long text should this be but there is just no time.")
-
-    pdf.add_bar_chart(170, 60, labels, data, 0, 60)
-    pdf.add_paragraph("Long text should this be but there is just no time.")
-
-    pdf.add_pie_chart(80, 80, labels, data[0])
-    pdf.add_pie_chart(80, 80, labels, data[1], side_labels=True)
-    pdf.add_paragraph("Long text should this be but there is just no time.")
+    store_address(db, pdf)
+    rentals_by_day(db, pdf, wb, start_date, end_date)
+    top_ten_customers_by_count(db, pdf, wb, start_date, end_date)
+    top_ten_actors(db, pdf, wb, start_date, end_date)
 
     pdf.save()
+    wb.save("sample.xlsx")
     return 0
